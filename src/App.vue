f5aec2691bfa1c12d557ff40492484d2f05475e8
Maurice Pittet <maurice.pittet@gmail.com>
2020-11-03 11:21:02 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <employe v-model="idemploye"  :msg="msg"></employe>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Employe from './components/Employe.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Employe
+  },
+  data: () => ({
+    idemploye: 666,
+    msg: 'toto'
+  }),
+  mounted () {
+    setTimeout(() => {
+      this.msg = 'tutu'
+    }, 10000)
   }
 }
 </script>
---
+++ src/components/Employe.vue
@@ -0,0 +1,66 @@
+<template>
+  <h3>{{ msg }} {{ setMyValue() }}</h3>
+  <div>idemploye:&nbsp;{{ idemploye }}</div>
+  <div>my_obj:&nbsp;{{ my_obj }}</div>
+  <div>title:&nbsp;{{ title }}</div>
+</template>
+
+<script>
+import { ref, reactive, onMounted, watchEffect } from 'vue'
+
+export default {
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: 0,
+      required: true
+    },
+    msg: {
+      type: String,
+      default: '',
+      required: false
+    }
+  },
+  setup(props) {
+    const title = ref('Merci patron')
+    const idemploye = ref(0)
+    const my_id = ref(999)
+    const my_obj = reactive({
+      firstname: 'Maurice',
+      lastname: 'Pittet',
+      idemploye: 10958
+    })
+
+    setTimeout(() => {
+      title.value = "THIS IS A NEW TITLE";
+      my_obj.idemploye = props.modelValue
+    }, 5000);
+
+    watchEffect(() => {
+      console.log('msg is: ', props.msg)
+    })
+
+    function setVal () {
+
+      return my_id.value + 1
+    }
+
+     onMounted(() => {
+      idemploye.value = props.modelValue
+      my_obj.idemploye = props.modelValue
+    })
+
+    return {
+      title,
+      idemploye,
+      my_id,
+      my_obj,
+      setMyValue: setVal,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
