f377ee4f1aa296c97942a3cd4598926f621dd2f5
Maurice Pittet <maurice.pittet@gmail.com>
2020-11-24 14:47:17 +0100

    New tests

--- src/components/Employe.vue
+++ src/components/Employe.vue
@@ -3,10 +3,21 @@
   <div>idemploye:&nbsp;{{ idemploye }}</div>
   <div>my_obj:&nbsp;{{ my_obj }}</div>
   <div>title:&nbsp;{{ title }}</div>
+  <div>{{ `x: ${pos.x}, y: ${pos.y}` }}</div>
 </template>
 
 <script>
 import { ref, reactive, onMounted, watchEffect } from 'vue'
+
+function useMousePosition() {
+  const pos = reactive({
+    x: 0,
+    y: 0
+  })
+
+  // ...
+  return pos
+}
 
 export default {
   props: {
@@ -27,6 +38,7 @@ export default {
     }
   },
   setup(props) {
+    const pos = useMousePosition()
     const title = ref('Merci patron')
     const idemploye = ref(0)
     const my_id = ref(999)
@@ -37,8 +49,10 @@ export default {
     })
 
     setTimeout(() => {
-      title.value = "THIS IS A NEW TITLE";
+      title.value = "THIS IS A NEW TITLE"
       my_obj.idemploye = props.modelValue
+      pos.x = 100
+      pos.y = 200
     }, 5000);
 
     watchEffect(() => {
@@ -60,6 +74,7 @@ export default {
     })
 
     return {
+      pos,
       title,
       idemploye,
       my_id,
